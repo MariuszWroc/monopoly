e198271363531cca7b36ef4c0201547f6553a0e3
Mariusz Czarny <mariusz.czarny88@gmail.com>
2017-03-19 21:40:21 +0100

    Set path for thymeleaf

--- src/main/java/com/monopoly/context/MvcConfig.java
+++ src/main/java/com/monopoly/context/MvcConfig.java
@@ -9,37 +9,63 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.monopoly"})
+@ComponentScan(basePackages = { "com.monopoly" })
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/contact").setViewName("contact");
-        registry.addViewController("/aboutme").setViewName("aboutme");
-        registry.addViewController("/login").setViewName("login");
-    }
-    
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-    
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
-        registry.addResourceHandler("/webapp/**").addResourceLocations("/webapp/").setCachePeriod(31556926);
-    }
-    
-    @Bean
-    public InternalResourceViewResolver getInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/pages/");
-        resolver.setSuffix(".html");
-        return resolver;
-    }
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/contact").setViewName("contact");
+		registry.addViewController("/aboutme").setViewName("aboutme");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(31556926);
+		registry.addResourceHandler("/webapp/**").addResourceLocations("/webapp/").setCachePeriod(31556926);
+	}
+
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".html");
+		return resolver;
+	}
+
+	@Bean(name = "templateResolver")
+	public ServletContextTemplateResolver getTemplateResolver() {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/pages/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("XHTML");
+		return templateResolver;
+	}
+
+	@Bean(name = "templateEngine")
+	public SpringTemplateEngine getTemplateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(getTemplateResolver());
+		return templateEngine;
+	}
+
+	@Bean(name = "viewResolver")
+	public ThymeleafViewResolver getViewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(getTemplateEngine());
+		return viewResolver;
+	}
 }
